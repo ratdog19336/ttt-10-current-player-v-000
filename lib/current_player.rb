96b8a889d8b460bfcfb6a62ae4fce1aaa3785d44
Nicholas Ratti <nickratti@gmail.com>
2019-08-23 20:12:00 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,1 +1,15 @@
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+
+def turn_count( board)
+  #returns number of turns played
+  counter = 0
+  board.each do |element|
+    if element == "X" || element == "O"
+      counter += 1
+    else
+    end
+end
+
+def current_player( turn_count)
+  #returns the current player based on turn counter
+end
